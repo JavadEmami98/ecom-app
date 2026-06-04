174afd01a32a733c505967b30d00480713f0991a
JavadEmami98 <javademami77@gmail.com>
2026-06-04 13:55:46 +0430

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <Routes>
+      
+    </Routes>
+      <p>Hi I'm Jay</p>
+    </>
+  );
+}
+
+export default App;
